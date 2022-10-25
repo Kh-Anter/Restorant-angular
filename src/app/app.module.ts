1ddffb01ad05877e3f2654da72da1bcc8a3fcfc0
Kh-Anter <105430867+Kh-Anter@users.noreply.github.com>
2022-10-25 03:26:23 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CategoriesComponent } from './body/categories/categories.component';
+import { AboutusComponent } from './body/aboutus/aboutus.component';
+import { ShopComponent } from './body/shop/shop.component';
+import { OfferComponent } from './body/offer/offer.component';
+import { BestDeliveryComponent } from './body/best-delivery/best-delivery.component';
+import { ReviewsComponent } from './body/reviews/reviews.component';
+import { AlloffersComponent } from './body/alloffers/alloffers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CategoriesComponent,
+    AboutusComponent,
+    ShopComponent,
+    OfferComponent,
+    BestDeliveryComponent,
+    ReviewsComponent,
+    AlloffersComponent
   ],
   imports: [
     BrowserModule,
